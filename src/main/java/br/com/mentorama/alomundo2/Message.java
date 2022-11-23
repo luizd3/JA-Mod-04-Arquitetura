cb2f61f12775c3d67607d505458b2064a3fcc453
Luiz Dorça <luizdorca@gmail.com>
2022-11-22 21:56:48 -0300

    Final da aula 7. Criando endpoints para retornar informação GET.

---
+++ src/main/java/br/com/mentorama/alomundo2/Message.java
@@ -0,0 +1,28 @@
+package br.com.mentorama.alomundo2;
+
+public class Message {
+
+    private Integer id;
+    private String message;
+
+    public Message(Integer id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
--- src/main/java/br/com/mentorama/alomundo2/OlaMundoController.java
+++ src/main/java/br/com/mentorama/alomundo2/OlaMundoController.java
@@ -4,15 +4,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/olamundo")
 public class OlaMundoController {
 
-    private final String message = "Olá Mundo";
+    private final List<Message> messages;
+
+    public OlaMundoController() {
+        this.messages = new ArrayList<>();
+        Message message1 = new Message(1, "Olá Mundo");
+        Message message2 = new Message(2, "Hello World");
+        Message message3 = new Message(3, "Terceira Mensagem");
+        this.messages.add(message1);
+        this.messages.add(message2);
+        this.messages.add(message3);
+
+    }
 
     @GetMapping
-    public String olaMundo(){
-        return message;
+    public List<Message> olaMundo(){
+        return messages;
     }
 
 }
