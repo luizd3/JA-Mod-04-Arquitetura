1b542c42935c9a521793d91b716e872b0d667760
Luiz Dorça <luizdorca@gmail.com>
2022-11-22 22:24:23 -0300

    Final da aula 8. Solicitando modificações ao servidor. POST, PUT, DELETE

--- src/main/java/br/com/mentorama/alomundo2/OlaMundoController.java
+++ src/main/java/br/com/mentorama/alomundo2/OlaMundoController.java
@@ -1,8 +1,6 @@
 package br.com.mentorama.alomundo2;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +13,39 @@ public class OlaMundoController {
 
     public OlaMundoController() {
         this.messages = new ArrayList<>();
-        Message message1 = new Message(1, "Olá Mundo");
-        Message message2 = new Message(2, "Hello World");
-        Message message3 = new Message(3, "Terceira Mensagem");
-        this.messages.add(message1);
-        this.messages.add(message2);
-        this.messages.add(message3);
+//        Message message1 = new Message(1, "Olá Mundo");
+//        Message message2 = new Message(2, "Hello World");
+//        Message message3 = new Message(3, "Terceira Mensagem");
+//        this.messages.add(message1);
+//        this.messages.add(message2);
+//        this.messages.add(message3);
 
     }
 
     @GetMapping
     public List<Message> olaMundo(){
         return messages;
+    }
+
+    @PostMapping
+    public Integer add(@RequestBody final Message message) {
+        if (message.getId() == null) {
+            message.setId(messages.size() + 1);
+        }
+        messages.add(message);
+        return message.getId();
+    }
+
+    @PutMapping
+    public void update(@RequestBody Message message) {
+         messages.stream()
+                 .filter(msg -> msg.getId().equals(message.getId()))
+                 .forEach(msg -> msg.setMessage(message.getMessage()));
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        messages.removeIf(msg -> msg.getId().equals(id));
     }
 
 }
